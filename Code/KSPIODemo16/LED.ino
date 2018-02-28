c1b6b4d412aa4021c6f0b84e45a7a1604f8a39b6
Tommy Esposito <35857516+tpespos@users.noreply.github.com>
2018-02-27 18:40:16 -0600

    Tommy's dim and undim functions

--- Code/KSPIODemo16/LED.ino
+++ Code/KSPIODemo16/LED.ino
@@ -7,12 +7,20 @@ void LEDsetup() {
   initialize(strip);
 }
 
+
+
+
+
 void colorChange(uint32_t c) {
   for (int i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
   }
   strip.show();
 }
+
+
+
+
 
 void colorBlink(uint32_t c, uint32_t delaytime) {
   for (int i = 0; i < strip.numPixels(); i++) {
@@ -27,6 +35,10 @@ void colorBlink(uint32_t c, uint32_t delaytime) {
   delay(delaytime);
 }
 
+
+
+
+
 void colorWipe(uint32_t c, uint8_t wait) {
   for (uint16_t i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
@@ -34,6 +46,31 @@ void colorWipe(uint32_t c, uint8_t wait) {
     delay(wait);
   }
 }
+
+
+
+void colorDim(uint32_t wait) {
+  for(uint16_t i=strip.getBrightness(); i>0; i--) {
+    strip.setBrightness(i);
+    strip.show();
+    delay(wait/256);
+  }
+}
+
+
+
+
+void colorUndim(uint32_t wait) {
+  for(uint16_t i=strip.getBrightness()+1; i<255; i++) {
+    strip.setBrightness(i+1);
+    strip.show();
+    delay(wait/256);
+  }
+}
+
+
+
+
 
 void dim(Adafruit_NeoPixel strip, uint32_t interval) {
   uint32_t timestep = interval / 15;
